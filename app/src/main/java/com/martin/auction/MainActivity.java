07c9c120d6b48b0c70445838b03c5f27456543fb
Vlad <11martinblack11@gmail.com>
2017-05-21 14:33:48 +0300

    go

---
+++ app/src/main/java/com/martin/auction/MainActivity.java
@@ -0,0 +1,75 @@
+package com.martin.auction;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button loadButton;
+    private ImageView image;
+    private EditText edtTxtPrice;
+    private EditText edtTxtDesc;
+    private static final int REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        image = (ImageView) findViewById(R.id.imageView);
+        loadButton = (Button) findViewById(R.id.button);
+        edtTxtPrice = (EditText) findViewById(R.id.editTextPrice);
+
+        loadButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //  getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(Intent.ACTION_PICK);
+        i.setType("image/*");
+        startActivityForResult(i, REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Bitmap img = null;
+
+        if (requestCode == REQUEST && resultCode == RESULT_OK) {
+            Uri selectedImage = data.getData();
+            try {
+                img = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            image.setImageBitmap(img);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void onClickEdtBox(View v) {
+        if (edtTxtPrice.getText().toString().equals("@string/price"))
+            edtTxtPrice.setText("");
+    }
+}
