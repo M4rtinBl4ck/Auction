c885b4aed401521c40a56004543627a6f0fce3c9
Vlad <11martinblack11@gmail.com>
2017-05-21 18:26:20 +0300

    Theme, actionBar

--- app/src/main/java/com/martin/auction/MainActivity.java
+++ app/src/main/java/com/martin/auction/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -84,4 +85,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
             edtTxtDesc.setTextColor(Color.BLACK);
         }
     } */
+
+    public void onImageClick (View v) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Скористайтеся кнопкою 'Завантажити зображення'", Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
